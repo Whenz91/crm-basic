c031b526eaa9f190b24906f10584344f91f6d14b
Zoltan Miko <mikozoltan91@gmail.com>
2020-02-18 21:56:41 +0100

    created read, create, read one, update functions

--- main.js
+++ main.js
@@ -1,7 +1,67 @@
+var editItem;
 $(document).ready(function() {
+    // load all user data from db
     getAllUsers();
 
-function getAllUsers() {
+    // create new user when I save the form data and refres the user list
+    $("#create").click(function() {
+        createNewUser();
+        $('#userModal').modal('hide');
+    });
+
+    $("#addUser").click(function() {
+        $("#userModalTitle").text("Új munkatárs felvétele");
+        $("#create").removeClass("d-none");
+        $("#update").addClass("d-none");
+    });
+
+    // read the clicked userprofile data from db and open modal
+    editItem = function(id) {
+        $("#userModalTitle").text("Adatok módosítása");
+        $("#update").removeClass("d-none");
+        $("#create").addClass("d-none");
+        getOneUser(id);
+    }
+
+    // send updated user data to the server
+    $("#update").click(function() {
+        updateUserData();
+    });
+
+
+    // collect data into one json and send back the server
+    function createNewUser() {
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var phone = $("#phone").val();
+        var location = $("#location").val();
+        var password = $("#password").val();
+        var rank = $('input[name=userRank]:checked', '#userForm').val();
+
+        var requestObject = {
+            name: name,
+            email: email,
+            phone: phone,
+            location: location,
+            password: password,
+            rank: rank
+        };
+        
+        $.ajax({
+            type: "POST",
+            url: "http://localhost/hanna-crm/users/create.php",
+            data: JSON.stringify(requestObject),
+            success: function (data) {
+                getAllUsers();
+            },
+            error: function () {
+                alert("Hiba történt az adatbázis elérésénél.");
+            }
+        });	
+    }
+
+    // get all user data from the db and show the front-end
+    function getAllUsers() {
         $.ajax({
             type: "GET",
             url: "http://localhost/hanna-crm/users/read.php",
@@ -34,13 +94,13 @@ function getAllUsers() {
                                     <i class='fas fa-city'></i> ${records[i].location}
                                 </p>
                                 <p class="card-text">
-                                    <i class='fas fa-phone'></i> +36- ${records[i].phone}
+                                    <i class='fas fa-phone'></i> +36 ${records[i].phone}
                                 </p>
                                 <p class="card-text">
                                     <i class='fas fa-envelope'></i> ${records[i].email}
                                 </p>
-                                <button type="button" class="btn btn-success" name="edit" id="${records[i].id}"><i class='fas fa-edit'></i></button>
-                                <button type="button" class="btn btn-danger" name="delete" id="${records[i].id}"><i class='fas fa-trash-alt'></i></button>
+                                <button type="button" class="btn btn-success edit" onclick='editItem("${records[i].id}")'><i class='fas fa-edit'></i></button>
+                                <button type="button" class="btn btn-danger delete" onclick='deleteItem("${records[i].id}")'><i class='fas fa-trash-alt'></i></button>
                             </div>
                         </div>
                     </div>
@@ -52,6 +112,75 @@ function getAllUsers() {
                 console.log("Hiba történt az adatbázis elérésénél.");
             }
         });
+    }
+
+    function getOneUser(id) {
+    
+        $.ajax({
+            type: "GET",
+            url: "http://localhost/hanna-crm/users/read_one.php?id=" + id + "",
+            success: function (data) {
+                $('#hiddenId').val(data.id);
+                $('#name').val(data.name);
+                $('#email').val(data.email);
+                $('#phone').val(data.phone);
+                $('#location').val(data.location);
+                $('#password').val(data.password);
+                switch (data.rank) {
+                    case "1":
+                        $("#userForm").find(":radio[name=userRank][value='1']").prop("checked", true);
+                        break;
+                    case "2":
+                        $("#userForm").find(":radio[name=userRank][value='2']").prop("checked", true);
+                        break;
+                    case "3":
+                        $("#userForm").find(":radio[name=userRank][value='3']").prop("checked", true);
+                        break;
+                    default:
+                        console.log("Hiba");
+                        break;
+                }
+
+                $('#userModal').modal('show');
+            },
+            error: function () {
+                alert("Hiba történt az adatbázis elérésénél.");
+            }
+        });	
+        
+    }
+
+    function updateUserData() {
+        var id = $("#hiddenId").val();
+        var email = $("#email").val();
+        var phone = $("#phone").val();
+        var location = $("#location").val();
+        var password = $("#password").val();
+        var rank = $('input[name=userRank]:checked', '#userForm').val();
+
+        var requestObject = {
+            id: id,
+            name: name,
+            email: email,
+            phone: phone,
+            location: location,
+            password: password,
+            rank: rank
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "http://localhost/hanna-crm/users/update.php",
+            data: JSON.stringify(requestObject),
+            success: function (data) {
+                getAllUsers();
+            },
+            error: function () {
+                alert("Hiba történt az adatbázis elérésénél.");
+            }
+        });	
+
+        $('#userModal').hide();
     }
 
 });
