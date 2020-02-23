f7e54e9d10aacbdabd25f806217b09a8a580ad7d
Zoltan Miko <mikozoltan91@gmail.com>
2020-02-23 15:06:05 +0100

    make some litle changeds

--- registration.php
+++ registration.php
@@ -11,6 +11,7 @@ if(!isset($_SESSION['userName'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/6c09380308.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/css/style.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Munkatársak - HannaInstCRM</title>
 </head>
@@ -61,64 +62,68 @@ if(!isset($_SESSION['userName'])) {
 
     <!-- Modal -->
     <div class="modal fade" id="userModal" tabindex="-1" role="dialog" aria-labelledby="userModalTitle" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="userModalTitle">Új munkatárs felvétele</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="modal-body">
-            <form method="POST" id="userForm">
-                <input type="text" class="d-none" id="hiddenId" value="">
-                <div class="form-group">
-                    <label for="name">Teljes név</label>
-                    <input type="text" class="form-control" id="name" name="name" value="">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="userModalTitle">Új munkatárs felvétele</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
-                <div class="form-group">
-                    <label for="email">Email cím</label>
-                    <input type="email" class="form-control" id="email" name="email" value="">
+                <div class="modal-body">
+                    <form method="POST" id="userForm">
+                        <input type="text" class="d-none" id="hiddenId" value="">
+                        <div class="form-group">
+                            <label for="name">Teljes név</label>
+                            <input type="text" class="form-control" id="name" name="name" value="">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email cím</label>
+                            <input type="email" class="form-control" id="email" name="email" value="">
+                        </div>
+                        <div class="form-group">
+                            <label for="phone">Telefonszám</label>
+                            <input type="text" class="form-control" id="phone" name="phone" value="">
+                        </div>
+                        <div class="form-group">
+                            <label for="location">Város</label>
+                            <input type="text" class="form-control" id="location" name="location" value="">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Jelszó</label>
+                            <input type="password" class="form-control" id="password" name="password" value="">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text">
+                                    <input type="checkbox" id="showPass" aria-label="Checkbox for following text input">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="userRank" id="admin" value="1" checked>
+                            <label class="form-check-label" for="admin">Admin</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="userRank" id="trader" value="2">
+                            <label class="form-check-label" for="trader">Kereskedő</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="userRank" id="mechanic" value="3">
+                            <label class="form-check-label" for="mechanic">Szervizes</label>
+                        </div>
+                        <small class="form-text text-muted">
+                            Az Admin rank a legmagasabb és mindent megtehet. A kereskedő és a szervizes korlátozott jogokkal rendelkezik.
+                        </small>
+                        <br>
+                    </form>
                 </div>
-                <div class="form-group">
-                    <label for="phone">Telefonszám</label>
-                    <input type="text" class="form-control" id="phone" name="phone" value="">
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Ablak bezárása</button>
+                    <button type="button" id="create" class="btn btn-primary">Munkatárs felvétele</button>
+                    <button type="button" id="update" class="btn btn-primary d-none">Adatok módosítása</button>
                 </div>
-                <div class="form-group">
-                    <label for="location">Város</label>
-                    <input type="text" class="form-control" id="location" name="location" value="">
-                </div>
-                <div class="form-group">
-                    <label for="password">Jelszó</label>
-                    <input type="password" class="form-control" id="password" name="password" value="">
-                </div>
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="userRank" id="admin" value="1" checked>
-                    <label class="form-check-label" for="admin">Admin</label>
-                </div>
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="userRank" id="trader" value="2">
-                    <label class="form-check-label" for="trader">Kereskedő</label>
-                </div>
-                <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="userRank" id="mechanic" value="3">
-                    <label class="form-check-label" for="mechanic">Szervizes</label>
-                </div>
-                <small class="form-text text-muted">
-                    Az Admin rank a legmagasabb és mindent megtehet. A kereskedő és a szervizes korlátozott jogokkal rendelkezik.
-                </small>
-                <br>
-            </form>
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Ablak bezárása</button>
-            <button type="button" id="create" class="btn btn-primary">Munkatárs felvétele</button>
-            <button type="button" id="update" class="btn btn-primary d-none">Adato módosítása</button>
-        </div>
+            </div>
         </div>
     </div>
-    </div>
-    
 
     <script
     src="https://code.jquery.com/jquery-3.4.1.min.js"
@@ -126,6 +131,6 @@ if(!isset($_SESSION['userName'])) {
     crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="main.js"></script>
+    <script src="assets/js/main.js"></script>
 </body>
 </html>
