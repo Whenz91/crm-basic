9d51f83e451e9c7db2416f39614235faad293853
Zoltan Miko <mikozoltan91@gmail.com>
2020-02-24 21:26:09 +0100

    cut pieces the dashbord view

--- dashbord.php
+++ dashbord.php
@@ -5,60 +5,14 @@ require_once "error_handle.php";
 if(!isset($_SESSION['userName'])) {
     header("location: index.php?error=nologedin");
 }
-$ErrorHandle = new ErrorHandle();
 
-?><!DOCTYPE html>
-<html lang="hu">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/6c09380308.js" crossorigin="anonymous"></script>
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Főoldal - HannaInstCRM</title>
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">
-            <img src="https://hannainst.hu/image/catalog/hanna-logo-250x80-min.jpeg" width="125" height="40" alt="logo">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+include "view/header.php";
+?>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="dashbord.php">Főoldal <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Profilom</a>
-                </li>
-                <?php
-                    if($_SESSION['userRank'] == 1) {
-                        echo '
-                        <li class="nav-item">
-                            <a class="nav-link" href="registration.php">Új munkatárs felvétele</a>
-                        </li>
-                        ';
-                    }
-                ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="logout.php">Kilépés</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    <?php include "view/nav.php"; ?>
+    
 
     <header class="container py-5">
-        <div class="col-7 mx-auto py-1">
-            <?php 
-                if(isset($_GET['message'])) {
-                    $ErrorHandle->messageCheck($_GET['message']);
-                }                    
-            ?>
-        </div>
-
         <div class="row">
             <a href="#" class="btn btn-lg btn-primary" role="button" aria-pressed="true"><i class='fas fa-address-card'></i> Új ügyfél felvétele</a>
         </div>
@@ -128,8 +82,4 @@ $ErrorHandle = new ErrorHandle();
     </main>
 
 
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php include "view/footer.php"; ?>
