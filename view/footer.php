ac2488314d4839cf3fdcd15cb463313f5db9a304
Zoltan Miko <mikozoltan91@gmail.com>
2020-03-02 21:16:45 +0100

    created switch to check what page is loaded and add the correct script file to the footer

--- view/footer.php
+++ view/footer.php
@@ -4,7 +4,28 @@
     crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="assets/js/main.js"></script>
-    <script src="assets/js/customerForm.js"></script>
+    <?php 
+            $page = $_SERVER["PHP_SELF"];
+            $pagePices = explode("/", $page);
+            $endPageUrlPices = end($pagePices);
+            
+            switch ($endPageUrlPices) {
+                case 'dashbord.php':
+                    echo '<script src="assets/js/customerRead.js"></script>';
+                break;
+                case 'registration.php':
+                    echo '<script src="assets/js/main.js"></script>';
+                break;
+                case 'profil.php':
+                    echo '';
+                break;
+                case 'customer.php':
+                    echo '<script src="assets/js/customerForm.js"></script>';
+                break;
+                default:
+                    echo '';
+                break;
+            }
+        ?>
 </body>
 </html>
